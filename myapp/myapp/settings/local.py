799eaa5cf8c2da8a0650778167047ff551004ae9
Chen Xu <chen.xu@appdynamics.com>
2017-06-10 02:20:42 -0700

    more

--- myapp/myapp/settings/local.py
+++ myapp/myapp/settings/local.py
@@ -2,8 +2,6 @@ from myapp.settings.common import *
 
 DEBUG = True
 
-SECRET_KEY = 'mb19)yeti1&m!2j=4kjqda%5pj*h4yvhk@n$q2&u+=sj9%@yp('
-
 STATIC_ROOT = "/srv/myproject/public/static/"
 MEDIA_ROOT = "/srv/myproject/public/media/"
 
