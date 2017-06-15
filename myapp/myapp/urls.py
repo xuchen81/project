8d3fdad98711a8d29b1761e3ed9c83449c199987
Chen Xu <chen.xu@appdynamics.com>
2017-06-14 23:09:31 -0700

    more

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+
+from reliability.views import PingView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^ping/?$', PingView.as_view(), name='ping'),
 ]
